d29b97a0a4beb08c7c2ebc822af933c03b26777f
Emil Nilsson <emini215@student.liu.se>
2016-09-05 18:07:24 +0200

    Load styles and scripts from folders

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.use("/styles", express.static(__dirname + "/styles"));
+app.use("/scripts", express.static(__dirname + "/scripts"));
+
 http.listen(3000, function() {
     console.log("Listening on port 3000.");
 });
