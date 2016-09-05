325a0f9983d15e2ebbdf394928bd8edf57bf5660
Emil Nilsson <emini215@student.liu.se>
2016-09-05 20:58:40 +0200

    Add basic socket.io usage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -8,6 +9,21 @@ app.get("/", function(req, res) {
 
 app.use("/styles", express.static(__dirname + "/styles"));
 app.use("/scripts", express.static(__dirname + "/scripts"));
+
+io.on("connection", function(socket) {
+    console.log("New connection.");
+    console.log(socket);
+    
+    // nothing really needed for disconnections, yet at least
+    socket.on("disconnect", function() {
+        console.log("Disconnected.");
+    });
+
+    socket.on("draw", function(x, y) {
+        console.log("fill " + x + "," + y);
+    });
+
+});
 
 http.listen(3000, function() {
     console.log("Listening on port 3000.");
