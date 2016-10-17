661842f02599cc62d81f868d93eb825a256b10a4
Emil Nilsson <karl.emil.nilsson@gmail.com>
2016-10-17 17:33:21 +0200

    Create app.js to start app, using part of old server.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-var app = require("./server.js");
-var PORT = 3000;
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var port = 3000;
 
-app.listen(PORT, function() {
-    console.log("Listening on port " + PORT + ".");
+// serve site
+app.get("/", function(req res) {
+    res.sendFile(__dirname + "/client/index.html");
 });
 
+// set path for serving scripts and style
+app.use("/scripts", express.static(__dirname + "/client/scripts"));
+app.use("/style", express.static(__dirname + "/client/style"));
+
+
+require("./server/index.js").listen(http);
+
+http.listen(port, function() {
+    console.log("Listening on port " + port + ".");
+});
